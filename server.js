592bc41ef57e2d7ac61a24cfb92ba4069daad07f
Matthew Songer <matt91893@gmail.com>
2022-05-21 17:02:42 -0500

    replaced files

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {}, 
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -18,21 +19,19 @@ const sess = {
   })
 };
 
+app.use(session(sess));
 
+const hbs = exphbs.create({});
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.use(session(sess));
-// turn on routes
-app.use(routes);
 
-// turn on connection to db and server
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
